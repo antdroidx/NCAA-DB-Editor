359689720c0a653b918f0b809e4097d669784b0e
Anthony Nguyen <ahnguyen@gmail.com>
2025-08-21 15:04:51 -0700

    Modernize WinForms startup

--- NCAA_XDBE/Program.cs
+++ NCAA_XDBE/Program.cs
@@ -11,8 +11,7 @@ namespace DB_EDITOR
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
             Application.Run(new MainEditor());
         }
     }
